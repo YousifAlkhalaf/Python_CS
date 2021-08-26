b8f1aefd97dae1962414020f2e86ac3d2e89fd44
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2021-08-26 14:38:08 -0700

    t

---
+++ Unit 3/3.2/Problem 1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 26 14:27:52 2021
+
+@author: Computer Science ~ B
+"""
+
+num = int(input('Enter a positive integer: '))
+
+for i in range(1, 13, 1):
+    print (f'{i} times {num} is {i*num}')
---
+++ Unit 3/3.2/Problem 2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 26 14:34:00 2021
+
+@author: Computer Science ~ B
+"""
+
+num = int(input('Enter a positive integer: '))
+total = 0
+f = num
+if num == 0:
+    print('0! is 1')
+else:
+    
+    
