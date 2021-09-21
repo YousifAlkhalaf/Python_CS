b3e56eb289d86fc581c4b8fccfcaa32fd49d661a
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2021-09-21 14:40:41 -0700

    nathan stop asking me about my middle name

---
+++ GUI Practice/Buttons.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root = Tk()
+
+def myClick():
+    myLabel = Label(root, text = 'Look! I clicked a Button!!!')
+    myLabel.pack()
+
+myButton = Button(root, text = 'Click Me!', command = myClick, fg = '#ffffff', bg = '#000000')
+myButton.pack()
+
+root.mainloop()
--- GUI Practice/Grid.py
+++ GUI Practice/Grid.py
@@ -2,10 +2,14 @@ from tkinter import *
 root = Tk()
 
 # Creating a Label Widget
-myLabel1 = Label(root, text = 'Hello World!').grid(row=0, column=0)
-myLabel2 = Label(root, text = 'My Name Is John Elder').grid(row=1, column=5)
+#myLabel1 = Label(root, text = 'Hello World!').grid(row=0, column=0)
+#myLabel2 = Label(root, text = 'My Name Is John Elder').grid(row=1, column=0)
+myLabel1 = Label(root, text = 'Hello World!')
+myLabel2 = Label(root, text = 'My Name Is John Elder')
+myLabel3 = Label(root, text = ' ')
 # Shoving it onto the screen
-#myLabel1.grid(row=0, column=0)
-#myLabel2.grid(row=1, column=5)
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row = 1, column=5)
+myLabel3.grid(row = 1, column = 1)
 
 root.mainloop()
