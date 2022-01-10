e2dd4a39abc0e96eeae95e2e3868bc6b54110909
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-01-10 13:46:51 -0800

    Update #5 - SoftwareShop.py

--- Review/Unit 2/#5 - SoftwareShop.py
+++ Review/Unit 2/#5 - SoftwareShop.py
@@ -4,6 +4,8 @@ Created on Thu Jan  6 14:35:25 2022
 
 @author: Computer Science ~ B
 """
+import math
+
 def findDiscount(num_bought):
     '''Calculates the discount given'''
     if num_bought >= 10 and num_bought <= 19:
@@ -16,9 +18,33 @@ def findDiscount(num_bought):
         return 0.5
     else:
         return 1
+
+def findCost(num_bought, pack_price, discount):
+    '''Calculates end cost'''  
+    return (num_bought * pack_price) * discount
     
 
 pack_price = 99
 num_bought = int(input('Enter the number of packages purchased: '))
 
 #Find discount
+discount = findDiscount(num_bought)
+discount_percent = int((1-discount) * 100)
+cost = findCost(num_bought, pack_price, discount)
+print(f'Your discount is {discount_percent}%')
+print('Your cost is ${:.2f}'.format(cost))
+
+#MPC extra
+new_amt = math.ceil(num_bought/10) * 10
+new_cost = findCost(new_amt, pack_price, findDiscount(new_amt))
+
+old_overall = cost/num_bought
+new_overall = new_cost/new_amt
+if new_overall < old_overall:
+    savings = (old_overall * num_bought - new_overall * new_amt)
+    more_packs = new_amt - num_bought
+    print('If you purchase {} more package(s), you will save ${:.2f}'.format(more_packs, savings))
+    
+    
+    
+    
