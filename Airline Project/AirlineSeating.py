ac2e0962aba36c05d4198c861193cafb06ac1e60
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-04-05 14:37:54 -0700

    numa numa noooooo

--- Airline Project/AirlineSeating.py
+++ Airline Project/AirlineSeating.py
@@ -2,29 +2,39 @@
 
 class Passenger(object):
     
-    def __init__(self, fn, ln, seat_class, position):
-        self.fn = fn
-        self.ln = ln
-        self.seat_class = seat_class
-        self.position = position
+    # Makes a Passenger according to a split line
+    def __init__(self, info_list):
+        self.fn = info_list[0]
+        self.ln = info_list[1]
+        self.seat_class = info_list[2]
+        self.position = info_list[3]
         
         self.seat = None
     
     def __str__(self):
-        return f'{self.fn} {self.ln} Seat {self.seat[0]}{self.seat[1]}'
+        if self.seat == None:
+            return f'{self.fn} {self.ln}: No seat assigned'
+        return f'{self.fn} {self.ln}: Seat {self.seat[0]}{self.seat[1]}'
     
+    def __repr__(self):
+        return self.__str__()
+    
+    # Returns passenger seat class
     def get_class(self):
         return self.seat_class
     
+    # Returns passenger seat position
     def get_position(self):
         return self.position
     
+    # Sets passenger's seat to a given seat tuple
     def set_seat(self, seat):
         self.seat = seat
 
 class Seats():
     taken_seats = []
     
+    # Creates a list of numbers for rows within a given seat class
     def find_row_range(seat_class):
         if seat_class == 'First':
             row_range = [i for i in range(1, 4)]
@@ -32,6 +42,7 @@ class Seats():
             row_range = [i for i in range(6, 31)]
         return row_range
     
+    # Creates a tuple of viable letters based on desired seat position
     def find_letter_group(position):
         if position == 'W':
             return ('A', 'F')
@@ -40,27 +51,32 @@ class Seats():
         else:
             return ('B', 'E')
     
-    def choose_seats(passengers):
-        if type(passengers) != list:
-            row_range = Seats.find_row_range(passengers.get_class())
-            letter_group = Seats.find_letter_group(passengers.get_position())
-            
-            for num in row_range:
-                for letter in letter_group:
-                    seat = (num, letter)
-                    
-                    if seat not in Seats.taken_seats:
-                        passengers.set_seat(seat)
-                        Seats.taken_seats.append(seat)
-                        return seat
-                
+    def choose_seats(party):
+        row_range = Seats.find_row_range(party[0].get_class())
         
-    
- 
-with open('passengers.txt', encoding='utf-8') as f:
+            
+
+passengers = []
+with open ('passengers.txt', encoding='utf-8') as f:
     for line in f:
         line = line.split()
-        passenger = Passenger(line[0], line[1], line[2], line[3])
+        passenger = Passenger(line)
+        is_new_party = int(line[4]) != 0 
         
-        Seats.choose_seats(passenger)
-        print(passenger)
+        if is_new_party:
+            # New party is created
+            passengers.append([passenger])
+        else:
+            # Passenger is put into last created party
+            passengers[-1].append(passenger)
+
+for party in passengers:
+    Seats.choose_seats(party)
+
+print(passengers)
+
+    
+    
+
+        
+
